e1123633dd2e30c177e9aff6b7c7ca0f72ab29a9
Alberto Coppini <skioppetto@gmail.com>
2021-05-23 23:47:38 +0200

    refactoring init test

--- test/desktop/tank_level/test_tank_level.cpp
+++ test/desktop/tank_level/test_tank_level.cpp
@@ -2,11 +2,16 @@
 #include<tank_level.h>
 
 // tank_level need to be initiated passing at least the height, the min_height and number of levels. 
+
+void init_tank_level(TankLevel &tl, int height, int minHeight, int levels){
+    tl.setHeightCm(height);
+    tl.setMinHeightCm(minHeight);
+    tl.setLevels(levels);
+}
+
 void test_tank_level_init(){
     TankLevel tl;
-    tl.setHeightCm(100);
-    tl.setMinHeightCm(10);
-    tl.setLevels(4);
+    init_tank_level(tl, 100, 10, 4);
     TEST_ASSERT_EQUAL_INT(100, tl.getHeightCm());
     TEST_ASSERT_EQUAL_INT(10, tl.getMinHeightCm());
     TEST_ASSERT_EQUAL_INT(4, tl.getLevels());
