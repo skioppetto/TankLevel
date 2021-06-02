6f9aa25081d9561991d71ef8da84818a4175727c
Alberto Coppini <skioppetto@gmail.com>
2021-06-03 00:01:10 +0200

    transmitter cannot trigger on hc sr04 until it receive a measure plus a lag of 60ms Fixes #11

--- lib/hcsr04_range/hcsr04_range.cpp
+++ lib/hcsr04_range/hcsr04_range.cpp
@@ -5,6 +5,8 @@ HCSR04Range::HCSR04Range(int trig, int echo)
     this->trig = trig;
     this->echo = echo;
     this->interval = 0ul;
+    this->ready = false;
+    this->last_measure_ms = 0ul;
     pinMode(trig, OUTPUT);
     pinMode(echo, INPUT);
 }
@@ -13,18 +15,28 @@ HCSR04Range::~HCSR04Range()
 {
 }
 
+bool HCSR04Range::checkMinCycle(){
+    return (last_measure_ms == 0 || millis() - last_measure_ms >= HCSR04_MIN_CYCLE_MS);
+}
+
 void HCSR04Range::trigger(){
-    //noInterrupts();     // disable interrupts to be more precise during pulse measure
-    digitalWrite(trig, LOW);
-    digitalWrite(trig, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trig, LOW);
-    interval = pulseIn(echo, HIGH);
-    //interrupts();       // reenable after returning a value
+    if (checkMinCycle()){
+        //noInterrupts();     // disable interrupts to be more precise during pulse measure
+        digitalWrite(trig, LOW);
+        digitalWrite(trig, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(trig, LOW);
+        interval = pulseIn(echo, HIGH);
+        //interrupts();       // reenable after returning a value
+        last_measure_ms = millis();
+        ready = true;
+    }
 }
 
 bool HCSR04Range::isReady(){
-    return interval > 0ul;
+    bool eval =  interval > 0ul && (checkMinCycle() || ready);
+    ready = false;
+    return eval;
 }
 
 unsigned long HCSR04Range::getIntervalMicros(){
---
+++ src/poc/hcsr04_issue11/hcsr04_issue11.cpp
@@ -0,0 +1,29 @@
+#ifdef POC_HCSR04_MINCYCLE
+#include<Arduino.h>
+#include<hcsr04_range.h>
+
+const int HCSR04_TRIGGER_PIN =3;
+const int HCSR04_ECHO_PIN = 4;
+
+HCSR04Range hc(HCSR04_TRIGGER_PIN, HCSR04_ECHO_PIN);
+
+void setup(){
+    Serial.begin(9600);
+}
+
+unsigned long last;
+
+void loop(){
+    hc.trigger();
+    if (hc.isReady()){
+        last = millis();
+        Serial.print("was ready, range uS: ");
+        Serial.println(hc.getIntervalMicros());
+    }else{
+        unsigned long diff = millis() - last;
+        Serial.print("not ready, should be less than 60ms: ");
+        Serial.println(diff);
+    }
+    delay(10);
+}
+#endif
