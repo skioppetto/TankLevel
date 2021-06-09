238b72e2728c3be90d613d714e47c7ce05cd1614
Alberto Coppini <skioppetto@gmail.com>
2021-06-09 23:36:38 +0200

    cannot compile HysteresisLevel on board PoC Fixes #28

--- lib/tank_level/hysteresis_level.h
+++ lib/tank_level/hysteresis_level.h
@@ -4,9 +4,9 @@ private:
     int hiLevel, loLevel;
 public:
     static const int DEFAULT_THRESHOLD = 5; 
-    static const int UNCHANGED = 0;
-    static const int LOW = -1;
-    static const int HIGH = 1;
+    static const int LEVEL_UNCHANGED = 0;
+    static const int LEVEL_LOW = -1;
+    static const int LEVEL_HIGH = 1;
     HysteresisLevel();
     HysteresisLevel(int level, int threshold); // threshold used to evaluate upper and lower level in hysteresis
     int eval(int measure);
