9a5dd5c00c77acfb7a5f1adf8a832213a1f592d3
Alberto Coppini <skioppetto@gmail.com>
2021-06-12 23:14:26 +0200

    Fixes consider to remove arduino uni tests #15

--- test/desktop/tank_level/test_tank_level.h
+++
@@ -1,8 +0,0 @@
-void test_tank_level_init();
-void test_set_measure();
-void test_calculate_levels_4();
-void test_calculate_levels_5();
-void test_calculate_levels_8_height_80();
-void test_update_levels_runtime();
-void test_hysteresis_level_up();
-void test_hysteresis_level_down();
