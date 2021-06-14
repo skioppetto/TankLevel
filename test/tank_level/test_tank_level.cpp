bd586e224c9bb94f96e6fdf9519463ffc06e83c1
Alberto Coppini <skioppetto@gmail.com>
2021-06-14 23:39:25 +0200

    Consider to create a constructor for Tank Level with all required fields Fixes #37

--- test/tank_level/test_tank_level.cpp
+++ test/tank_level/test_tank_level.cpp
@@ -2,15 +2,13 @@
 #include<tank_level.h>
 
 // tank_level need to be initiated passing at least the height, the min_height and number of levels. 
-void init_tank_level(TankLevel *tl, int height, int minHeight, int levels){
-    tl->setHeight(height);
+void init_tank_level(TankLevel *tl, int minHeight){
     tl->setMinHeight(minHeight);
-    tl->setLevels(levels);
 }
 
 void test_tank_level_init(){
-    TankLevel tl;
-    init_tank_level(&tl, 100, 10, 4);
+    TankLevel tl(100, 4);
+    init_tank_level(&tl, 10);
     TEST_ASSERT_EQUAL_INT(100, tl.getHeight());
     TEST_ASSERT_EQUAL_INT(10, tl.getMinHeight());
     TEST_ASSERT_EQUAL_INT(4, tl.getLevels());
@@ -19,7 +17,7 @@ void test_tank_level_init(){
 
 // measure set
 void test_set_measure(){
-    TankLevel tl; 
+    TankLevel tl(100, 4); 
     tl.setMeasure (50);
     TEST_ASSERT_EQUAL_INT(50, tl.getMeasure());
 }
@@ -30,8 +28,8 @@ void test_calculate_level(TankLevel &tl, int measure, int level){
 }
 
 void test_calculate_levels_4(){
-    TankLevel tl;
-    init_tank_level(&tl, 100, 10, 4);
+    TankLevel tl(100, 4);
+    init_tank_level(&tl, 10);
    test_calculate_level(tl, 102, TankLevel::LEVEL_OVERFLOW); 
     test_calculate_level(tl, 91, TankLevel::LEVEL_LOW);
     test_calculate_level(tl, 78, 1);
@@ -41,8 +39,8 @@ void test_calculate_levels_4(){
 }
 
 void test_calculate_levels_5(){
-    TankLevel tl;
-    init_tank_level(&tl, 100, 10, 5);
+    TankLevel tl(100, 5);
+    init_tank_level(&tl, 10);
     test_calculate_level(tl, 91, TankLevel::LEVEL_LOW);
     test_calculate_level(tl, 85, 1);
     test_calculate_level(tl, 66, 2);
@@ -52,8 +50,8 @@ void test_calculate_levels_5(){
 }
 
 void test_calculate_levels_8_height_80(){
-    TankLevel tl;
-    init_tank_level(&tl, 80, 5, 8);
+    TankLevel tl(80, 8);
+    init_tank_level(&tl, 5);
     test_calculate_level(tl, 97, TankLevel::LEVEL_OVERFLOW);
     test_calculate_level(tl, 79, TankLevel::LEVEL_LOW);
     test_calculate_level(tl, 75, 1);
@@ -67,8 +65,8 @@ void test_calculate_levels_8_height_80(){
 }
 
 void test_update_levels_runtime(){
-    TankLevel tl;
-    init_tank_level(&tl, 80, 5, 8);
+    TankLevel tl(80, 8);
+    init_tank_level(&tl, 5);
     //tl.setHysteresis(2);
     test_calculate_level(tl, 44, 4);
     test_calculate_level(tl, 27, 6);
@@ -79,8 +77,8 @@ void test_update_levels_runtime(){
 }
 
 void test_hysteresis_level_up(){
-    TankLevel tl;
-    init_tank_level(&tl, 80, 5, 8);
+    TankLevel tl(80, 8);
+    init_tank_level(&tl, 5);
     // step between levels is 10, set an hysteresis of 2
     tl.setHysteresis(2);
     // first level is freely set, level 4 is from 50 to 40
@@ -94,8 +92,8 @@ void test_hysteresis_level_up(){
 }
 
 void test_hysteresis_level_down(){
-    TankLevel tl;
-    init_tank_level(&tl, 80, 5, 8);
+    TankLevel tl(80, 8);
+    init_tank_level(&tl, 5);
     // step between levels is 10, set an hysteresis of 2
     tl.setHysteresis(2);
     // first level is freely set, level 4 is from 50 to 40
@@ -108,10 +106,16 @@ void test_hysteresis_level_down(){
     //test_calculate_level(tl, 45, 4); 
 }
 
+void test_tank_level_init_level_0(){
+    TankLevel tl(80, 0);
+    TEST_ASSERT_EQUAL(1, tl.getLevels()); // test levels has min value 1 
+}
+
 int main(int argc, char const *argv[])
 {
     UNITY_BEGIN();
     RUN_TEST(test_tank_level_init);
+    RUN_TEST(test_tank_level_init_level_0);
     RUN_TEST(test_set_measure);
     RUN_TEST(test_calculate_levels_4);
     RUN_TEST(test_calculate_levels_5);
