6e038b538eb479105bff0908bdeb6564cf540a54
Alberto Coppini <skioppetto@gmail.com>
2021-06-20 15:01:58 +0200

    the receiver when tuned on should show the current level on a led based screen Fixes #48

--- src/tank_level_receiver/tank_level_receiver.cpp
+++ src/tank_level_receiver/tank_level_receiver.cpp
@@ -2,15 +2,36 @@
 #include<radio_message.h>
 #include <RH_ASK.h>
 #include <SPI.h> // Not actualy used but needed to compile
+#include <Adafruit_NeoPixel.h>
+
+#define NEOPIXEL_PIN 10
 
 RH_ASK driver;
 RadioMessage rmReceived;
+Adafruit_NeoPixel strip(7, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
+
+void colorWipe(uint32_t c, uint8_t wait, unsigned int pixels) {
+  strip.clear(); 
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+      if (i > 0){
+      if (i <= pixels)
+        strip.setPixelColor(i, strip.Color(255, 0, 0));
+      else 
+        strip.setPixelColor(i, strip.Color(0, 00, 0));  
+      }
+    strip.show();
+    delay(wait);
+  }
+}
 
 void setup(){
     #ifdef DEBUG
     Serial.begin(9600);
     #endif
     driver.init(); // TODO: should I do something if driver init fail? see #53
+    strip.begin();
+    strip.setBrightness(50);
+    strip.clear(); // Initialize all pixels to 'off'
 }
 
 void loop(){
@@ -28,5 +49,6 @@ void loop(){
         Serial.print("\tvalue: ");
         Serial.println(rmReceived.getValue());
         #endif   
+        colorWipe(strip.Color(255, 0, 0), 50, rmReceived.getValue());
     }
 }
